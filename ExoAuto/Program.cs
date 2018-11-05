91e7127211f767dd5252984013cbe78cc0264245
nbl241 <39767922+nbl241@users.noreply.github.com>
2018-11-05 14:38:39 +0100

    Exo auto

--- ExoAuto/Program.cs
+++ ExoAuto/Program.cs
@@ -10,6 +10,29 @@ namespace ExoAuto
     {
         static void Main(string[] args)
         {
+            int mat = 0;
+
+            Voiture voiture = new Voiture
+            {
+                Matricule = mat++,
+                AnneeModele = "2018",
+                Prix = 6000
+            };
+
+            Console.WriteLine("Matricule: {0}, Année: {1}, Prix: {2}", voiture.Matricule, voiture.AnneeModele, voiture.Prix);
+            Console.WriteLine("{0} et {1} !!", voiture.Demarrer(), voiture.Accelerer());
+
+            Camion camion = new Camion
+            {
+                Matricule = mat++,
+                AnneeModele = "2016",
+                Prix = 12000
+            };
+
+            Console.WriteLine("Matricule: {0}, Année: {1}, Prix: {2}", camion.Matricule, camion.AnneeModele, camion.Prix);
+            Console.WriteLine("{0} et {1} !!", camion.Demarrer(), camion.Accelerer());
+
+            Console.ReadLine();
         }
     }
 }
---
+++ ExoAuto/Vehicule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoAuto
+{
+    public abstract class Vehicule
+    {
+        private int matricule;
+        private string anneeModele;
+        private Double prix;
+
+        public int Matricule { get => matricule; set => matricule = value; }
+
+        public string AnneeModele { get => anneeModele; set => anneeModele = value; }
+
+        public double Prix { get => prix; set => prix = value; }
+
+        public override string ToString()
+        {
+            return "Matricule = " + Matricule + ", Année = " + AnneeModele + ", Prix = " + Prix;
+        }
+
+        public abstract string Demarrer();
+
+        public abstract string Accelerer();
+    }
+}
